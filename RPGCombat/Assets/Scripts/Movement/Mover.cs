e2135d3a076b342ed3db0621fd69fdd2d4307054
Bibek Pandey <bibekpandey365@gmail.com>
2022-10-17 13:44:44 +0545

    New Commit

--- RPGCombat/Assets/Scripts/Movement/Mover.cs
+++ RPGCombat/Assets/Scripts/Movement/Mover.cs
@@ -1,35 +1,55 @@
+using RPG.Core;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+
+namespace RPG.Movement
 {
-
-    void Start()
+    public class Mover : MonoBehaviour, IAction
     {
-        
+        NavMeshAgent navMeshAgent;
+
+        void Start()
+        {
+            navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        void Update()
+        {
+            UpdateAnimator();
+        }
+
+        public void StartMoveAction(Vector3 destination)
+        {
+            GetComponent<ActionScheduler>().StartAction(this);
+            
+            MoveTo(destination);
+        }
+
+        public void MoveTo(Vector3 destination)
+        {
+            navMeshAgent.destination = destination;
+            navMeshAgent.isStopped = false;
+        }
+
+        public void Cancel()
+        {
+            navMeshAgent.isStopped = true;
+        }
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = navMeshAgent.velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+
+            float speed = localVelocity.z;
+
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
+
     }
-
-    void Update()
-    {
-        UpdateAnimator();
-    }
-
-    public void MoveTo(Vector3 destination)
-    {
-        GetComponent<NavMeshAgent>().destination = destination;
-    }
-
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-
-        float speed = localVelocity.z;
-
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
-    }
-
 }
+
